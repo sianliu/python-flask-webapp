5681725d121cf33764a02959c354c428fba71af9
Jon Liu <mr@sianliu.com>
2020-02-07 00:42:01 +0800

    Initial commit: building a web application with python and flask

---
+++ __init__.py
@@ -0,0 +1,31 @@
+ import os
+
+ from flask import Flask
+
+ def create_app(test_config=None):
+     app = Flask(__name__)
+     app.config.from_mapping(
+         SECRET_KEY=os.environ.get(SECRET_KEY, default=dev),
+     )
+
+     if test_config is None:
+         app.config.from_pyfile(config.py, silent=True)
+     else:
+         app.config.from_mapping(test_config)
+
+     from .models import db
+     db.init_app(app)
+
+     @app.route('/')
+      def index():
+         return redirect(url_for('tickets'))
+
+      @app.route('/tickets')
+       def tickets():
+         return render_template('tickets_index.html')
+
+      @app.route('/tickets/<int:ticket_id>')
+       def tickets_show(ticket_id):
+         return render_template('tickets_show.html')
+
+     return app
